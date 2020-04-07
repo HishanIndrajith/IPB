cf86825792aeb0eccca0811ca413cff51277f5ba
Hishan Indrajith Adikari <hishan.indrajith.95hia@gmail.com>
2020-04-07 13:59:00 +0000

    Upload New File

---
+++ mobility/ArrayToRaster.py
@@ -0,0 +1,26 @@
+import gdal, ogr, os, osr
+import numpy as np
+
+
+def array2raster(newRasterfn,rasterOrigin,pixelWidth,pixelHeight,array):
+
+    cols = array.shape[1]
+    rows = array.shape[0]
+    originX = rasterOrigin[0]
+    originY = rasterOrigin[1]
+
+    driver = gdal.GetDriverByName('GTiff')
+    outRaster = driver.Create(newRasterfn, cols, rows, 1, gdal.GDT_Float32)
+    outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
+    outband = outRaster.GetRasterBand(1)
+    outband.WriteArray(array)
+    outRasterSRS = osr.SpatialReference()
+    outRasterSRS.ImportFromEPSG(4326)
+    outRaster.SetProjection(outRasterSRS.ExportToWkt())
+    outband.FlushCache()
+
+
+def main(newRasterfn,rasterOrigin,pixelWidth,pixelHeight,array):
+    reversed_arr = array[::-1] # reverse array so the tif looks like the array
+    array2raster(newRasterfn,rasterOrigin,pixelWidth,pixelHeight,reversed_arr) # convert array to raster
+
